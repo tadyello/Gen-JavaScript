937551a538ca594b78cc68f5b02f1e1e9a76bb8c
tadyello <tadyello@hotmail.com>
2022-09-08 11:50:53 -0300

    Exercício Javascript feito no bootcamp Generation.

---
+++ Aula 2/index.js
@@ -0,0 +1,5 @@
+var variavel = 45 //Inteiro
+var variavel = 4.5 //Real
+var variavel = "Programadores Cariocas"//
+var variavel = "A"//
+var variavel = false //
---
+++ Lista de Exercicios/Exercicio 2/index.js
@@ -0,0 +1,8 @@
+let nome, peso, dataNascimento, altura, dataInternacao, diabetes
+nome = prompt("Digite o nome do paciente: ");
+peso = prompt("Digite o peso do paciente: ");
+dataNascimento = prompt("Digite a data de nascimento do paciente: ");
+altura = prompt("Digite a altura do paciente: ");
+dataInternacao = prompt("Digite data que o paciente foi internado: ");
+diabetes = prompt("O paciente é diabético? ");
+alert("Nome do paciente: "+ nome + "\nPeso: "+ peso +" KG" + "\nData de nascimento: " + dataNascimento + "\nAltura: " + altura + "\nData de internação do paciente: " + dataInternacao + "\nPaciente diabético: "+ diabetes);
