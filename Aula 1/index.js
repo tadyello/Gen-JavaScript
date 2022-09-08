f34a34c593e3a05c9580574e3c5eb164b78b6987
tadyello <tadyello@hotmail.com>
2022-09-08 09:03:13 -0300

    Atividades em JavaScript feitas no bootcamp Generation.

---
+++ Aula 1/index.js
@@ -0,0 +1,8 @@
+let nome,  idade, musica, filme, filhos, sonho
+nome = prompt("Escreva seu nome: ");
+idade = prompt("Qual a sua idade?");
+musica = prompt("Qual seu estilo musical favorito?");
+filme = prompt("Qual o seu filme favorito?");
+filhos = prompt("Quantos filhso você tem?");
+sonho = prompt("Qual o seu sonho?");
+alert("Seu nome é "+ nome + "\nVocê tem "+ idade +" anos" + "\nSeu estilo musical é "+ musica + "\nSeu filme favorito é " + filme + "\nVocê tem " + filhos +"filhos"+ "\nSeu sonho é " + sonho );
